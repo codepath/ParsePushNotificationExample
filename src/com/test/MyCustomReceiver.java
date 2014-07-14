fef17643cb5c39e1266906e83ad58275d801cee2
Nathan Esquenazi <nesquena@gmail.com>
2014-07-13 22:55:47 -0700

    Update MyCustomReceiver.java

--- src/com/test/MyCustomReceiver.java
+++ src/com/test/MyCustomReceiver.java
@@ -1,6 +1,5 @@
 package com.test;
 
-
 import java.util.Iterator;
 
 import org.json.JSONException;
@@ -13,49 +12,70 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 public class MyCustomReceiver extends BroadcastReceiver {
-	private static final String TAG = "MyCustomReceiver";
+   private static final String TAG = "MyCustomReceiver";
+   public static final String intentAction = "SEND_PUSH";
 
-	public static final String intentAction = "SEND_PUSH";
-	
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		try {
-			if (intent == null)
-			{
-				Log.d(TAG, "Receiver intent null");
-			}
-			else
-			{
-				String action = intent.getAction();
-				Log.d(TAG, "got action " + action );
-				if (action.equals(intentAction))
-				{
-					String channel = intent.getExtras().getString("com.parse.Channel");
-					JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
-
-					Log.d(TAG, "got action " + action + " on channel " + channel + " with:");
-					Iterator<String> itr = json.keys();
-					while (itr.hasNext()) {
-						String key = (String) itr.next();
-						if (key.equals("customdata"))
-						{
-							// Handle push notif by invoking activity directly
-							Intent pupInt = new Intent(context, ShowPopUp.class);
-							pupInt.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK );
-							pupInt.putExtra("customdata", json.getString(key));
-							context.getApplicationContext().startActivity(pupInt);
-							
-							// Handle push notif by sending a local braoadcast to which the activity 
-							// subscirbes to
-							LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(intentAction));
-						}
-						Log.d(TAG, "..." + key + " => " + json.getString(key));
-					}
-				}
-			}
-
-		} catch (JSONException e) {
-			Log.d(TAG, "JSONException: " + e.getMessage());
-		}
-	}
+   @Override
+   public void onReceive(Context context, Intent intent) {
+       if (intent == null) {
+           Log.d(TAG, "Receiver intent null");
+       } else {
+          // Parse push message and handle accordingly
+          processPush(context, intent);
+       }
+   }
+   
+   private void processPush(Context context, Intent intent) {
+       String action = intent.getAction();
+       Log.d(TAG, "got action " + action );
+       if (action.equals(intentAction))
+       {
+           String channel = intent.getExtras().getString("com.parse.Channel");
+           try {       	
+               JSONObject json = new JSONObject(intent.getExtras().getString("com.parse.Data"));
+               Log.d(TAG, "got action " + action + " on channel " + channel + " with:");
+               // Iterate the parse keys if needed
+               Iterator<String> itr = json.keys();
+               while (itr.hasNext()) {
+                   String key = (String) itr.next();
+         	   // Extract custom push data
+         	   if (key.equals("customdata")) {    
+         	 	// Handle push notification by invoking activity directly
+         		launchSomeActivity(context, json.getString(key));
+         		// OR trigger a broadcast to activity
+         		triggerBroadcastToActivity(context);
+         		// OR create a local notification
+         		createNotification(context);
+         	    }
+                    Log.d(TAG, "..." + key + " => " + json.getString(key));
+               }
+       	    } catch (JSONException ex) {
+       		Log.d(TAG, "JSON failed!");
+       	    }
+       }
+   }
+   
+   public static final int NOTIFICATION_ID = 45;
+   // Create a local dashboard notification to tell user about the event
+   private void createNotification(Context context) {
+       NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context).setSmallIcon(
+          		R.drawable.ic_launcher).setContentTitle("Successfully logged in");
+       NotificationManager mNotificationManager = (NotificationManager) context
+          		.getSystemService(Context.NOTIFICATION_SERVICE);
+       mNotificationManager.notify(45, mBuilder.build());
+   }
+   
+   // Handle push notification by invoking activity directly
+   private void launchSomeActivity(Context context, String datavalue) {
+       Intent pupInt = new Intent(context, ShowPopUpActivity.class);
+       pupInt.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK );
+       pupInt.putExtra("customdata", datavalue);
+       context.getApplicationContext().startActivity(pupInt);	
+   }
+   
+   // Handle push notification by sending a local broadcast 
+   // to which the activity subscribes to
+   private void triggerBroadcastToActivity(Context context) {
+       LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(intentAction));
+   }
 }
