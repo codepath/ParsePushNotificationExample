e211b954ae200152961f9dbea9c176d53b9a4f29
Roger Hu <roger.hu@gmail.com>
2018-05-01 23:29:16 -0700

    Bump versions

--- app/src/main/java/com/test/MainApp.java
+++ app/src/main/java/com/test/MainApp.java
@@ -4,8 +4,9 @@ import android.app.Application;
 
 import com.parse.Parse;
 import com.parse.ParseInstallation;
-import com.parse.interceptors.ParseLogInterceptor;
-import com.parse.interceptors.ParseStethoInterceptor;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 
 public class MainApp extends Application {
 
@@ -19,12 +20,20 @@ public class MainApp extends Application {
         super.onCreate();
 
         Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+        // Use for monitoring Parse OkHttp traffic
+        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
+        // See http://square.github.io/okhttp/3.x/logging-interceptor/ to see the options.
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(PARSE_APP_ID)
                 .clientKey(null) // no client key needed in Parse open source
                 .server(PARSE_CLOUD_SERVER_URL) // do not forget the URL needs to end with a trailing slash
-                .addNetworkInterceptor(new ParseStethoInterceptor())
-                .addNetworkInterceptor(new ParseLogInterceptor())
+                .clientBuilder(builder)
                 .build());
 
         // Need to register GCM token
